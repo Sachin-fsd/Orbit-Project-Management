b084b2306c8f5853ae6b6e8eac79a3145c70a0bd
Sachin-fsd <sachineducational555@gmail.com>
2025-06-14 23:40:30 +0530

    buf fix.

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,8 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
